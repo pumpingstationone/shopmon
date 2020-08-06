d34c1386fe923238701ddd5101bf03a7677843a1
Ron Olson <tachoknight@gmail.com>
2020-08-06 18:21:21 -0500

    Updated code to take into consideration of the 'ready to arm' message

--- sensors/sendevents.py
+++ sensors/sendevents.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import json
 import socket
 import logging
 import threading
@@ -10,6 +11,15 @@ from queue import Queue
 # Our work queue
 workQueue = Queue()
 
+# Holds our array of sensors; we read this
+# when the program starts as it won't change
+# very often
+sensors = {}
+
+# This thread reads the sensor data from the socket, not
+# the serial port, using https://github.com/nutechsoftware/ser2sock
+# so that the serial data can be used by multiple programs
+# concurrently
 def readData():
     logging.info("Starting to read data")
     global workQueue
@@ -34,7 +44,8 @@ def readData():
         if len(full_msg) > 0:
             workQueue.put(full_msg.strip())
 
-
+# This thread reads the messages that readData() above put
+# on the queue and sends them to the MQTT server.
 def readQueue():
     def parseLine(line):
         parts = line.split(',')
@@ -53,11 +64,22 @@ def readQueue():
             
             goodLine, parts = parseLine(line)
             if goodLine:
+                # Special situation: Zone 008 is "****DISARMED****  Ready to Arm  " which
+                # is not a sensor, so for right now we ignore this one
+                if parts[1] == "008":
+                    continue
+
+                # Now let's find the sensor number in the
+                # array...
+                sensorName = "Unknown sensor"
+                for sensor in sensors:
+                    if sensor['zone'] == parts[1]:
+                        sensorName = sensor['name']
                 # Now we send the timestamp and sensor
                 # number to the mqtt server
 
                 ts = round(time.time())
-                txline = str(ts) + "," + parts[1]
+                txline = str(ts) + "," + sensorName
                 logging.info("%s", txline)
 
                 # Set up our mqtt connection
@@ -70,6 +92,10 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
+
+    logging.info("Loading dictionary")
+    with open('sensors.json') as sensor_file:
+        sensors = json.load(sensor_file)
 
     logging.info("Creating thread to read data")        
     gdThread = threading.Thread(target=readData)
